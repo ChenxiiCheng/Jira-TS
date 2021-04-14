c902215af002429cf6b668dceb8c00488a3ec07d
ChenxiiCheng <chenxic1011@gmail.com>
2021-04-14 12:20:30 -0400

    feat: add useMount and useDebounce hooks

--- src/screens/project-list/index.js
+++ src/screens/project-list/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import qs from "qs";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { cleanObject } from "utils";
+import { cleanObject, useDebouncce, useMount } from "utils";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -13,24 +13,25 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = React.useState([]);
   const [users, setUsers] = React.useState([]);
+  const debouncedParam = useDebouncce(param, 2000);
 
-  React.useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, []);
+  });
 
   React.useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedParam]);
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -11,4 +13,22 @@ export const cleanObject = (object) => {
   });
 
   return result;
+};
+
+export const useMount = (callback) => {
+  React.useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebouncce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = React.useState(value);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
