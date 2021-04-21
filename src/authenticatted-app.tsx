2d0ed0dbf443d81a282c618668d57a4b83cc79ce
ChenxiiCheng <chenxic1011@gmail.com>
2021-04-21 13:46:34 -0400

    feat: use flexbox and grid system for ui layout

--- src/authenticatted-app.tsx
+++ src/authenticatted-app.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "@emotion/styled";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 
@@ -6,9 +7,66 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
 
   return (
-    <>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
+  grid-gap: 10rem;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`;
+
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+
+const Footer = styled.footer`
+  grid-area: footer;
+`;
